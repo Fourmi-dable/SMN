fda4fd001f857925783b7ab192b0261820c87be8
Fourmi-dable <fourmifourmi@proton.me>
2025-09-11 22:55:27 +0200

    feat: starts adding login avatar list window and cleans style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,33 +2,36 @@ import { io } from "socket.io-client";
 import './App.css'
 import "7.css/dist/7.css";
 import avatar from "./assets/butterfly.jpg"
+import { useState } from "react";
 
 function App() {
+  const [avatarListOpen, setAvatarListOpen] = useState(false);
   const socket = io(import.meta.env.VITE_SERVER_URL);
 
   console.log("Socket depuis le client:", socket);
 
   return (
-    <>
-      <div className="window active" style={{ width: "500px" }}>
-        <div className="title-bar">
-          <div className="title-bar-text">SMN Messenger - Se connecter</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize"></button>
-            <button aria-label="Maximize"></button>
-            <button aria-label="Close"></button>
-          </div>
-        </div>
-        <div className="window-body has-space main-container">
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "500px" }}>
+        <div className="main-container">
           <div className="avatar-container">
             <img src={avatar} alt="Logo" />
           </div>
-          <button className="blue-bg btn-small">Changer</button>
+          <button
+            className="blue-bg btn-small"
+            onClick={() => setAvatarListOpen(true)}
+          >
+            Changer
+          </button>
           <div className="group">
-            <label htmlFor="username">Pseudo:</label>
-            <input type="text" id="username" placeholder="Pseudo" />
-            <label htmlFor="status">Statut:</label>
-            <input type="text" id="status" placeholder="Statut" />
+            <div className="input-container">
+              <label htmlFor="username">Pseudo:</label>
+              <input type="text" id="username" placeholder="Pseudo" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="status">Statut:</label>
+              <input type="text" id="status" placeholder="Statut" />
+            </div>
             <div className="status-select">
               <label htmlFor="online">Etat:</label>
               <select id="online" defaultValue={"En ligne"}>
@@ -41,7 +44,34 @@ function App() {
           </div>
         </div>
       </div >
-    </>
+      {
+        avatarListOpen &&
+        <div className="window active" style={{ width: "500px", position: "absolute" }}>
+          <div className="title-bar">
+            <div className="title-bar-text">SMN Messenger - Choisis ton avatar</div>
+            <div className="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close" onClick={() => setAvatarListOpen(false)}></button>
+            </div>
+          </div>
+          <div className="window-body has-space" style={{ display: "flex", flexWrap: "wrap" }}>
+            <div className="avatar-container">
+              <img src={avatar} alt="Logo" />
+            </div>
+            <div className="avatar-container">
+              <img src={avatar} alt="Logo" />
+            </div>
+            <div className="avatar-container">
+              <img src={avatar} alt="Logo" />
+            </div>
+            <div className="avatar-container">
+              <img src={avatar} alt="Logo" />
+            </div>
+          </div>
+        </div >
+      }
+    </div >
   )
 }
 
